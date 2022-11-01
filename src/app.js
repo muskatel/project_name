62e331cadeab39f0d6d52cb94babd7ee3fa62a5d
Craig Marais <craig.marais.85@gmail.com>
2022-11-01 06:39:59 +0100

    Ready ...

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-console.log("Hello, world!")
+// load .env into process.env
+require('dotenv').config()
+
+// Minimalist web framework
+const express = require('express')
+
+// body parsing middleware
+const bodyParser = require('body-parser')
+
+// HTTP request logger middleware
+const morgan = require('morgan')
+
+// OS path module (part of Node)
+const path = require('path')
+
+// load PORT if it is specified otherwise use 8080
+const port = process.env.PORT || 8080
+
+// create the express app
+const app = express()
+
+// initialise morgan
+app.use(morgan('dev'))
+
+// initialise body parsing for json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// GET / -> sends src/welcome.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/welcome.html'))
+})
+
+// start the server
+app.listen(port, () => {
+  console.log('Server started on port:', port)
+})
